f52107d80c28f9362897fe3099e0fec5bde18b07
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2021-11-10 01:59:18 +0600

    feat: stats preview card component done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        veryDarkBlue: 'hsl(233, 47%, 7%)',
+        darkDesaturatedBlue: 'hsl(244, 38%, 16%)',
+        softViolet: 'hsl(277, 64%, 61%)'
+      },
+    },
   },
   variants: {
     extend: {},
